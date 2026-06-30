5cd21a6ddfb518da1bdae5c14fef541294ecf5b6
Rabia Williams <rabiawilliams@gmail.com>
2026-06-30 16:55:49 +1000

    removed redundant message extension lab

--- docs/pages/extend-message-ext/05-add-action.md
+++
@@ -1,3 +0,0 @@
-#  Lab M5 - Enhance plugin with an action command
-
-<img src="https://m365-visitor-stats.azurewebsites.net/copilot-camp/extend-message-ext/05-add-action" />
--- src/extend-message-ext/Lab04-SSO-Auth/Northwind/src/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  botId: process.env.BOT_ID,
-  botPassword: process.env.BOT_PASSWORD,
-  storageAccountConnectionString: process.env.STORAGE_ACCOUNT_CONNECTION_STRING,
-  connectionName: process.env.CONNECTION_NAME,
-};
-
-export default config;
